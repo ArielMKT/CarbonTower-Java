81fe62aafc4a04c441d0039c0de183852711f041
IgorMartns <igor.inacio.martins@outlook.com>
2019-05-20 20:48:13 -0300

    New login screen

--- StreamChampion/src/main/java/com/streamchampion/application/swing/Login.java
+++ StreamChampion/src/main/java/com/streamchampion/application/swing/Login.java
@@ -46,20 +46,21 @@ public class Login extends JFrame {
         TextFieldName = new JTextFieldHint(new JTextField(), "user-icon", "Login");
         TextFieldPassword = new JPassWordFieldHint(new JPasswordField(), "padlock", "Senha");
         btnLogin = new javax.swing.JButton();
-        btnLogout = new javax.swing.JButton();
         forgotPassword = new javax.swing.JLabel();
-        backgroundImage = new javax.swing.JLabel();
+        register = new javax.swing.JLabel(); //5
+        account = new javax.swing.JLabel(); //2
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
         setSize(new java.awt.Dimension(530, 330));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        mainPanel.setBackground(new java.awt.Color(45, 48, 53));
         mainPanel.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
             public void mouseDragged(java.awt.event.MouseEvent evt) {
                 jPanel2MouseDragged(evt);
             }
         });
+
         mainPanel.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jPanel2MouseClicked(evt);
@@ -73,19 +74,26 @@ public class Login extends JFrame {
 
         title.setFont(new java.awt.Font("Arial Narrow", 1, 24)); // NOI18N
         title.setForeground(new java.awt.Color(255, 255, 255));
-        title.setText("StreamChampion");
-        mainPanel.add(title, new org.netbeans.lib.awtextra.AbsoluteConstraints(59, 121, 165, -1));
+        title.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/logo-texto-deitado(FINO).png")));
+        mainPanel.add(title, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 290, 60));
 
+        TextFieldName.setBackground(new java.awt.Color(45, 48, 53));
         TextFieldName.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
-        mainPanel.add(TextFieldName, new org.netbeans.lib.awtextra.AbsoluteConstraints(126, 22, 150, 30));
+        TextFieldName.setForeground(new java.awt.Color(255, 51, 51));
 
+        mainPanel.add(TextFieldName, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 120, 230, 30));
+
+        TextFieldPassword.setBackground(new java.awt.Color(45, 48, 53));
         TextFieldPassword.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
-        mainPanel.add(TextFieldPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(309, 22, 150, 30));
+        TextFieldPassword.setForeground(new java.awt.Color(255,51,51));
 
-        btnLogin.setBackground(new java.awt.Color(58, 65, 84));
+
+        mainPanel.add(TextFieldPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 170, 230, 30));
+
+        btnLogin.setBackground(new java.awt.Color(245, 71, 71));
         btnLogin.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
         btnLogin.setForeground(new java.awt.Color(255, 255, 255));
-        btnLogin.setText("Entrar");
+        btnLogin.setText("Login");
         btnLogin.setBorderPainted(false);
         btnLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnLogin.setFocusPainted(false);
@@ -103,41 +111,34 @@ public class Login extends JFrame {
                 btnLoginActionPerformed(evt);
             }
         });
-        mainPanel.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 80, 120, 30));
-
-        btnLogout.setBackground(new java.awt.Color(255, 51, 51));
-        btnLogout.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
-        btnLogout.setForeground(new java.awt.Color(255, 255, 255));
-        btnLogout.setText("Sair");
-        btnLogout.setBorderPainted(false);
-        btnLogout.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnLogout.setFocusPainted(false);
-        btnLogout.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnLogoutActionPerformed(evt);
-            }
-        });
-        mainPanel.add(btnLogout, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 130, 120, 30));
+        mainPanel.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 250, 230, 30));
 
         forgotPassword.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         forgotPassword.setForeground(new java.awt.Color(255, 0, 0));
         forgotPassword.setText("Esqueci minha senha");
-        mainPanel.add(forgotPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 60, -1, -1));
+        mainPanel.add(forgotPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 210, -1, -1));
 
-        backgroundImage.setIcon(new javax.swing.ImageIcon("src/main/resources/icons/header-bg.jpg")); // NOI18N
-        mainPanel.add(backgroundImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 530, 330));
+        account.setFont(new java.awt.Font("Arial", 0, 12));
+        account.setForeground(new java.awt.Color(204, 204, 204));
+        account.setText("Não tem uma conta?");
+        mainPanel.add(account, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 300, -1, -1));
 
-        getContentPane().add(mainPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 530, 330));
+        register.setFont(new java.awt.Font("Arial", 0, 12));
+        register.setForeground(new java.awt.Color(255, 0, 0));
+        register.setText("Cadastre-se");
+        mainPanel.add(register, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 300, -1, -1));
+
+        getContentPane().add(mainPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 350, 370));
 
         pack();
     }// </editor-fold>
 
     private void btnLoginMouseEntered(java.awt.event.MouseEvent evt) {
-        btnLogin.setBackground(new Color(84, 95, 124));
+        btnLogin.setBackground(new Color(245, 81, 81));
     }
 
     private void btnLoginMouseExited(java.awt.event.MouseEvent evt) {
-        btnLogin.setBackground(new Color(58, 65, 84));
+        btnLogin.setBackground(new Color(245, 71, 71));
     }
 
     private void jPanel2MouseClicked(java.awt.event.MouseEvent evt) {
@@ -244,6 +245,8 @@ public class Login extends JFrame {
     private javax.swing.JLabel forgotPassword;
     private javax.swing.JPanel mainPanel;
     private Index index;
+    private javax.swing.JLabel register;
+    private javax.swing.JLabel account;
     // End of variables declaration
 }
 
